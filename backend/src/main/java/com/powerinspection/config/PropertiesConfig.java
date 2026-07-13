fda0644484509f6a9330d403b901367aa6dfa21a
@laonei <3073561696@qq.com>
2026-07-13 11:19:23 +0800

    feat(robot,wechat): 接入实机桥接并精简小程序功能
    Signed-off-by: @laonei <3073561696@qq.com>

--- backend/src/main/java/com/powerinspection/config/PropertiesConfig.java
+++ backend/src/main/java/com/powerinspection/config/PropertiesConfig.java
@@ -3,8 +3,9 @@ package com.powerinspection.config;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import com.powerinspection.model.ModelProperties;
+import com.powerinspection.robot.RobotProperties;
 
 @Configuration
-@EnableConfigurationProperties({JwtProperties.class, CorsProperties.class, ModelProperties.class})
+@EnableConfigurationProperties({JwtProperties.class, CorsProperties.class, ModelProperties.class, RobotProperties.class})
 public class PropertiesConfig {
 }
